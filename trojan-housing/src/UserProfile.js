1bdea4329ce86b17af7e3730c20ffa4942a77b9d
Leo Rosales <leorosal@usc.edu>
2024-04-26 15:39:15 -0700

    Updated Individual Page styling

--- trojan-housing/src/UserProfile.js
+++ trojan-housing/src/UserProfile.js
@@ -11,27 +11,27 @@ function UserProfile() {
   return (
     <div className="user-profile">
       <Navbar />
-      <div className = "user-profile-block">
+      <div className="user-profile-block">
 
-      <div className="user-info">
-        <img src={Avatar} alt="USER PROFILE PICTURE" className="user-avatar" />
-        <h1>{username}</h1>
-      </div>
+        <div className="user-info">
+          <img src={Avatar} alt="USER PROFILE PICTURE" className="user-avatar" />
+          <h1>{username}</h1>
+        </div>
 
-      <div className="interactions">
+        <div className="interactions">
 
-      <div className="saved-properties">
-        <h2>{username}'s Saved Properties</h2>
-        <SavedProperties />
-      </div>
-      <div className="user-comments">
-        <h2>{username}'s Saved Properties</h2>
-        <Comments />
-      </div>
+          <div className="saved-properties">
+            <h2>{username}'s Saved Properties</h2>
+            <SavedProperties />
+          </div>
+          <div className="user-comments">
+            <h2>{username}'s Saved Properties</h2>
+            <Comments />
+          </div>
 
-      </div>
+        </div>
 
-      
+
       </div>
 
     </div>
