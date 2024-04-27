ae8a6fb7722a9a7e502307800ddea7046f1c8316
Leo Rosales <leorosal@usc.edu>
2024-04-26 20:45:12 -0700

    linked user authentication to user profile page

--- trojan-housing/src/UserProfile.js
+++ trojan-housing/src/UserProfile.js
@@ -3,11 +3,13 @@ import Comments from './Comment';
 import SavedProperties from './SavedProperties';
 import Navbar from './Navbar';
 import Avatar from './images/user-avatar.jpg';
+import { useAuth } from './AuthenticationState';
 import './UserProfile.css';
 
 const username = "[USERNAME]"; // Username for all properties
 
 function UserProfile() {
+  const { user } = useAuth();
   return (
     <div className="user-profile">
       <Navbar />
@@ -15,17 +17,17 @@ function UserProfile() {
 
         <div className="user-info">
           <img src={Avatar} alt="USER PROFILE PICTURE" className="user-avatar" />
-          <h1>{username}</h1>
+          <h1>{user?.email}</h1>
         </div>
 
         <div className="interactions">
 
           <div className="saved-properties">
-            <h2>{username}'s Saved Properties</h2>
+            <h2>{user?.email}'s Saved Properties</h2>
             <SavedProperties />
           </div>
           <div className="user-comments">
-            <h2>{username}'s Saved Properties</h2>
+            <h2>{user?.email}'s Saved Properties</h2>
             <Comments />
           </div>
 
