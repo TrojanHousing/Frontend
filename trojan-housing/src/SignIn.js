2bbd7879d17f472d42f3c68f20d2b3664da69a39
zachdcali <97813419+zachdcali@users.noreply.github.com>
2024-05-05 16:44:07 -0700

    Sign In with error handling

--- trojan-housing/src/SignIn.js
+++ trojan-housing/src/SignIn.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthenticationState';
 import Navbar from './Navbar';
 import './SignIn.css';
 
 const SignIn = () => {
-  const navigate = useNavigate(); // Initialize navigate function
+  const navigate = useNavigate();
   const { signIn } = useAuth();
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
   const [errors, setErrors] = useState({});
+  const [loginError, setLoginError] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,6 +38,8 @@ const SignIn = () => {
       errors.password = 'Password is required';
     }
     setErrors(errors);
+    setLoginError('');
+
     if (Object.keys(errors).length === 0) {
       try {
         const response = await fetch('http://localhost:8080/userLogin', {
@@ -46,15 +49,25 @@ const SignIn = () => {
           },
           body: `email=${encodeURIComponent(formData.email)}&password=${encodeURIComponent(formData.password)}`,
         });
+        const errorText = await response.text(); // Get the response text, which might be an error code or message
         if (response.ok) {
           signIn(formData.email, formData.password);
           navigate('/');
         } else {
-          // Handle login error
-          console.error('Login failed');
+          switch (errorText) {
+            case '-1':
+              setLoginError('Invalid email. Please check your email and try again.');
+              break;
+            case '-2':
+              setLoginError('Incorrect password. Please try again.');
+              break;
+            default:
+              setLoginError('Login failed, please check your credentials and try again.');
+          }
         }
       } catch (error) {
         console.error('Error:', error);
+        setLoginError('An error occurred while trying to sign in. Please try again later.');
       }
     }
   };
@@ -88,6 +101,7 @@ const SignIn = () => {
               />
               {errors.password && <div className="error">{errors.password}</div>}
             </div>
+            {loginError && <div className="error login-error">{loginError}</div>}
             <button type="submit" className="button">Sign In</button>
           </form>
         </div>
